aa1d18348265d88ca7852e27000d02ef6b1440c8
masatomo-sugawara <masatomo.s1982@gmail.com>
2022-07-02 08:03:56 +0900

    Propsを知る

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ColofulMessage = (props) => {
+  // console.log(props);
+  const { color, children } = props;
+  const contentStyle = {
+    // オブジェクトのプロパティに対して変数で値を設定するとき、名前が同じであれば、変数名は省略できる
+    color,
+    fontSize: "18px"
+  };
+  return <p style={contentStyle}>{children}</p>;
+};
+
+export default ColofulMessage;
